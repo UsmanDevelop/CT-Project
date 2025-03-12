e444492a71d2f30fb56a56c8b0a3bcbe7572e7f8
Muhammad Usman Malik <usmanmalik9680@gmail.com>
2025-03-12 17:00:06 +0500

    Image Adjusted

--- src/Pages/HowItWorks.jsx
+++ src/Pages/HowItWorks.jsx
@@ -7,16 +7,16 @@ import forwarding from './../assets/FORWARDING.png'
 import finalDelivery from './../assets/FINALDELIVERY.png'
 import payment from './../assets/PAYMENTTRANSFER.png'
 import path from './../assets/PATH.png'
-
+import mobilePath from './../assets/PATHMOBILE.png'
 
 
 
 const steps = [
-  { title: "Pickup from Your Location", description: "Our rider picks up your parcel from your home or office.", img: pickup },
-  { title: "Secure Transit to Warehouse", description: "The parcel is safely stored in our warehouse before forwarding.", img: secure, },
-  { title: "Forwarding to Destination", description: "We process and send the parcel to the destination warehouse.", img: forwarding, },
-  { title: "Final Delivery Attempt", description: "Our rider delivers the parcel to your client, attempting 2-3 times if needed.", img: finalDelivery, },
-  { title: "Payment Transfer or Return", description: "Successful delivery? Your payment is transferred. If returned, the parcel comes back safely.", img: payment, },
+  { title: "Pickup from Your Location", description: "Our rider picks up your parcel from your home or office.", img: pickup, position: "xl:top-[-7rem] xl:left-[6rem] top-[-10rem] left-[-2rem]" },
+  { title: "Secure Transit to Warehouse", description: "The parcel is safely stored in our warehouse before forwarding.", img: secure, position: "xl:top-[-1rem] xl:left-[30rem] left-[-12rem] top-[5rem] sm:left-[-22rem] md:left-[-26rem] lg:left-[-30rem]" },
+  { title: "Forwarding to Destination", description: "We process and send the parcel to the destination warehouse.", img: forwarding, position: "xl:top-[10rem] xl:left-[-50rem] left-[15rem] top-[-2rem]" },
+  { title: "Final Delivery Attempt", description: "Our rider delivers the parcel to your client, attempting 2-3 times if needed.", img: finalDelivery, position: "xl:top-[-5rem] xl:left-[35rem] left-[-12rem] sm:left-[-22rem] md:left-[-26rem] lg:left-[-30rem] top-[9rem]" },
+  { title: "Payment Transfer or Return", description: "Successful delivery? Your payment is transferred. If returned, the parcel comes back safely.", img: payment, position: "xl:top-[4rem] xl:left-[-56rem] right-[-12rem] top-[2rem] sm:right-[-23rem] sm:top-[5rem]" },
 ];
 
 
@@ -33,19 +33,18 @@ function HowItWorks() {
 
         <div className="relative bg-black text-white py-10 px-5">
           <div className="max-w-5xl mx-auto relative">
-            {/* <div className="absolute top-1/2 left-5  m-4 w-full h-2 bg-gray-700 rounded-full hidden lg:block"></div> */}
 
-            <div className="flex flex-col md:flex-row md:flex-wrap md:justify-between">
-                  {/* <div className="w-6 h-6 bg-orange-500 rounded-full absolute -top-3 md:-left-3 md:top-1/2"></div> */}
-              <img src={path} className='absolute'/>
+            <div className="flex flex-row flex-wrap justify-between">
+              <img src={path} className='xl:visible absolute invisible'/>
+              <img src={mobilePath} className='xl:invisible absolute visible top-[2rem] left-[30%] h-sreen'/>
               {steps.map((step, index) => (
-                <div key={index} className={`relative flex flex-col items-center text-center p-5 md:w-1/2 lg:w-1/3 ${index % 2 === 0 ? "md:self-start" : "md:self-end"}`}>
+                <div key={index} className={`relative flex flex-col items-center text-center p-5 w-1/2 xl:w-1/3 ${step.position} ${index % 2 === 0 ? "md:self-start" : "md:self-end"} `}>
 
                   <ShapeBlur
-                    className={`h-[10px] overflow-hidden absolute hidden md:block`}
+                    className={`h-[10px] overflow-hidden absolute hidden xl:block`}
                     variation={0}
                     pixelRatioProp={window.devicePixelRatio || 1}
-                    shapeSize={1.9}
+                    shapeSize={1.6}
                     roundness={0.5}
                     borderSize={0.07}
                     circleSize={0.5}
@@ -53,7 +52,7 @@ function HowItWorks() {
                   />
                   <img src={step.img} alt={step.title} className="h-66 w-66 mb-[-80px]" />
                   <h3 className="font-bold text-lg mt-3">{step.title}</h3>
-                  <p className="text-gray-400 mt-2 text-sm text-center p-6 mr-10 ml-10">{step.description}</p>
+                  <p className="text-gray-400 mt-2 hidden xl:block text-sm text-center p-6 xl:mr-10 xl:ml-10">{step.description}</p>
                 </div>
               ))}
             </div>
