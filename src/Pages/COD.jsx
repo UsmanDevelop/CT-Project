1239e451c7793e9d01108362aa87a1c693ee789d
Muhammad Usman Malik <usmanmalik9680@gmail.com>
2025-03-15 17:44:22 +0500

    Adjusted COD page

--- src/Pages/COD.jsx
+++ src/Pages/COD.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import NavBar from './../Components/NavBar'
-// import { Globe } from "@/registry/magicui/globe";
+import Count from './../Components/Counter'
 function COD() {
   return (
     <>
@@ -12,13 +12,13 @@ function COD() {
         }} */}
       {/* > */}
       <section className="bg-black text-white  xl:h-screen">
-        <div className=" flex flex-col md:flex-col items-center w-full  h-screen align-middle justify-between">
+        <div className=" flex flex-col items-center w-full  h-screen align-middle justify-between">
           <NavBar />
 
           <div className='md:flex md:flex-row justify-between bg-black w-full md:p-19'>
-          {/* Left Side Content */}
+            {/* Left Side Content */}
             <div className="md:w-1/2 text-center md:text-left bg-black">
-            
+
               <div className='items-center text-center justify-center flex flex-col gap-5 mt-10'>
                 <h4 className="text-green-400 font-bold uppercase ">Rabbit Cash On Delivery</h4>
                 <h1 className="text-4xl md:text-5xl font-extrabold mt-4 xl:mb-10">
@@ -32,14 +32,36 @@ function COD() {
               </div>
 
               <div className="flex flex-col sm:flex-row mt-8 space-y-6 sm:space-y-0 sm:space-x-12">
-                <div>
-                  <h2 className="text-green-400 text-3xl font-bold">25,000+</h2>
-                  <p className="text-gray-400">Happy & Growing Partners</p>
+                
+                <div className='flex gap-15'>
+                  <div>
+                    <h2 className="text-green-400 text-3xl font-bold">
+                      <Count
+                        from={0}
+                        to={25000}
+                        separator=","
+                        direction="up"
+                        duration={1}
+                        className="count-up-text"
+                      />+
+                    </h2>
+                    <p className="text-gray-400">Happy & Growing Partners</p>
+                  </div>
+                  <div>
+                    <h2 className="text-green-400 text-3xl font-bold"><Count
+                      from={0}
+                      to={600}
+                      separator=","
+                      direction="up"
+                      duration={1}
+                      className="count-up-text"
+                    />+
+
+                    </h2>
+                    <p className="text-gray-400">Destinations</p>
+                  </div>
                 </div>
-                <div>
-                  <h2 className="text-green-400 text-3xl font-bold">600+</h2>
-                  <p className="text-gray-400">Destinations</p>
-                </div>
+
               </div>
 
               <div className="mt-8">
